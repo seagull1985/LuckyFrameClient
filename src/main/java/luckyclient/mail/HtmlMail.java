9c519cc9dfbd5e37f1dae7841448b8749ae15a4d
seagull1985 <heyanlin@qq.com>
2018-02-27 17:26:38 +0800

    回退 'Pull Request !2 : LuckyFrameClient Pull Request'

--- src/main/java/luckyclient/mail/HtmlMail.java
+++ src/main/java/luckyclient/mail/HtmlMail.java
@@ -12,57 +12,58 @@ import luckyclient.publicclass.remoterinterface.HttpClientHelper;
  * 为了尊重作者的劳动成果，LuckyFrame关键版权信息严禁篡改
  * 有任何疑问欢迎联系作者讨论。 QQ:1573584944  seagull1985
  * =================================================================
- *
+ * 
  * @author： seagull
  * @date 2017年12月1日 上午9:29:40
+ * 
  */
 public class HtmlMail {
-    static FreemarkerEmailTemplate fet = new FreemarkerEmailTemplate();
-
-    public static String htmlContentFormat(int[] taskcount, String taskid, String buildstatus, String restartstatus, String time, String jobname) {
-        Map<Object, Object> parameters = new HashMap<>(0);
-        parameters.put("buildstatus", buildstatus);
-        parameters.put("restartstatus", restartstatus);
-        parameters.put("casecount", taskcount[0]);
-        parameters.put("casesuc", taskcount[1]);
-        parameters.put("casefail", taskcount[2]);
-        parameters.put("caselock", taskcount[3]);
-        parameters.put("caseunex", taskcount[4]);
-        parameters.put("time", time);
-        parameters.put("taskid", taskid);
-        parameters.put("jobname", jobname);
-        try {
-            Map<String, String> headmsg = new HashMap<>(0);
-            Properties properties = luckyclient.publicclass.SysConfig.getConfiguration();
-            if ("ture".equals(properties.getProperty("task.push.switch"))) {
-                luckyclient.publicclass.LogUtil.APP.info("开始向第三方平台推送任务执行情况....");
-                Map<String, Object> pushparameters = new HashMap<>(0);
-                pushparameters.put("buildstatus", buildstatus);
-                pushparameters.put("restartstatus", restartstatus);
-                pushparameters.put("casecount", taskcount[0]);
-                pushparameters.put("casesuc", taskcount[1]);
-                pushparameters.put("casefail", taskcount[2]);
-                pushparameters.put("caselock", taskcount[3]);
-                pushparameters.put("caseunex", taskcount[4]);
-                pushparameters.put("time", time);
-                pushparameters.put("taskid", taskid);
-                pushparameters.put("jobname", jobname);
-
-                String pushurl = properties.getProperty("task.push.url");
-                HttpClientHelper.httpClientPostJson(pushurl, pushparameters, "utf-8", headmsg);
-            }
-        } catch (Exception e) {
-            luckyclient.publicclass.LogUtil.APP.error("向第三方平台推送任务执行情况出现异常，请检查！", e);
-            e.printStackTrace();
-            return fet.getText("task-body", parameters);
-        }
-        return fet.getText("task-body", parameters);
-    }
-
-    public static String htmlSubjectFormat(String jobname) {
-        Map<Object, Object> parameters = new HashMap<>(0);
-        parameters.put("jobname", jobname);
-        return fet.getText("task-title", parameters);
-    }
+	static FreemarkerEmailTemplate  fet=new FreemarkerEmailTemplate();
+	
+	public static String htmlContentFormat(int[] taskcount,String taskid,String buildstatus,String restartstatus,String time,String jobname){		
+		Map<Object, Object> parameters = new HashMap<Object, Object>(0);
+		parameters.put("buildstatus", buildstatus);
+		parameters.put("restartstatus", restartstatus);
+		parameters.put("casecount", taskcount[0]);
+		parameters.put("casesuc", taskcount[1]);
+		parameters.put("casefail", taskcount[2]);
+		parameters.put("caselock", taskcount[3]);
+		parameters.put("caseunex", taskcount[4]);
+		parameters.put("time", time);
+		parameters.put("taskid", taskid);
+		parameters.put("jobname", jobname);
+		try{
+			Map<String, String> headmsg = new HashMap<String, String>(0);
+			Properties properties = luckyclient.publicclass.SysConfig.getConfiguration();
+			if("ture".equals(properties.getProperty("task.push.switch"))){
+				luckyclient.publicclass.LogUtil.APP.info("开始向第三方平台推送任务执行情况....");
+				Map<String, Object> pushparameters = new HashMap<String, Object>(0);
+				pushparameters.put("buildstatus", buildstatus);
+				pushparameters.put("restartstatus", restartstatus);
+				pushparameters.put("casecount", taskcount[0]);
+				pushparameters.put("casesuc", taskcount[1]);
+				pushparameters.put("casefail", taskcount[2]);
+				pushparameters.put("caselock", taskcount[3]);
+				pushparameters.put("caseunex", taskcount[4]);
+				pushparameters.put("time", time);
+				pushparameters.put("taskid", taskid);
+				pushparameters.put("jobname", jobname);
+				
+				String pushurl=properties.getProperty("task.push.url");
+				HttpClientHelper.httpClientPostJson(pushurl, pushparameters, "utf-8", headmsg);
+			}				
+		}catch(Exception e){
+			luckyclient.publicclass.LogUtil.APP.error("向第三方平台推送任务执行情况出现异常，请检查！", e);
+			e.printStackTrace();
+			return fet.getText("task-body", parameters);
+		}
+			return fet.getText("task-body", parameters);
+	}
+	
+	public static String htmlSubjectFormat(String jobname){
+		Map<Object, Object> parameters = new HashMap<Object, Object>(0);
+		parameters.put("jobname", jobname);
+		return fet.getText("task-title", parameters);
+	}
 
 }
