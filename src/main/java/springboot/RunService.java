5440e6334c0e26ecbcc95d735112db9a6caead8d
seagull <24163551@qq.com>
2020-02-27 08:46:23 +0800

    优化netty配置的部分代码

--- src/main/java/springboot/RunService.java
+++ src/main/java/springboot/RunService.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import cn.hutool.core.util.BooleanUtil;
 import luckyclient.netty.NettyClient;
 import luckyclient.utils.config.SysConfig;
 
@@ -25,7 +26,7 @@ import luckyclient.utils.config.SysConfig;
 public class RunService {
 
 	private static final Logger log = LoggerFactory.getLogger(RunService.class);
-	private static final String NETTY_MODEL= SysConfig.getConfiguration().getProperty("netty.model");
+	private static final Boolean NETTY_MODEL= BooleanUtil.toBoolean(SysConfig.getConfiguration().getProperty("netty.model"));
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PropertyConfigurator.configure(System.getProperty("user.dir") + File.separator +"bootlog4j.conf");
@@ -36,9 +37,7 @@ public class RunService {
 		} catch (Exception e) {
 			log.error("获取服务IP出现异常......", e);
 		}
-		if(NETTY_MODEL==null||"".equals(NETTY_MODEL)||("0".equals(NETTY_MODEL)))
-			HttpImpl.checkHostNet();
-		else {
+		if(NETTY_MODEL){
 			try {
 				log.info("##################客户端netty开启#################");
 				NettyClient.start();
@@ -46,6 +45,8 @@ public class RunService {
 				log.error("连接服务端netty异常......");
 				e.printStackTrace();
 			}
+		}else{
+			HttpImpl.checkHostNet();
 		}
 	}
 
